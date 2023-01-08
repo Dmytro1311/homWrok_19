590151ae38231b00c2feddc40bfaea1781f4f69c
Your Name <you@example.com>
2023-01-08 19:48:03 +0200

    for example

---
+++ src/Account.java
@@ -0,0 +1,88 @@
+public class Account {
+    private String name;
+    private int dayOfBirth;
+    private int mounthOfBirth;
+    private int yearOfBirth;
+    private String email;
+    private String phoneNumber;
+
+
+    private String sureName;
+    private int weight;
+    private int pressure;
+    private int stepsByDay;
+    private int age;
+
+    Account( String name, int dayOfBirth, int mounthOfBirth,int yearOfBirth,String email,String phoneNumber,String sureName,
+     int weight, int pressure, int stepsByDay){
+        this.name = name;
+        this.dayOfBirth = dayOfBirth;
+        this.mounthOfBirth =mounthOfBirth;
+        this.yearOfBirth=yearOfBirth;
+        this.email=email;
+        this.phoneNumber =phoneNumber;
+        this.sureName = sureName;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.stepsByDay = stepsByDay;
+        this.age = 2020 - yearOfBirth;
+    }
+    public  String getName(){
+        return this.name;
+    }
+    public int getDayOfBirthday(){
+        return this.dayOfBirth;
+    }
+    public int getMounthOfBirthday(){
+        return this.mounthOfBirth;
+    }
+    public int getYearOfBirthday(){
+        return this.yearOfBirth;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setSureName(String sureName){
+        this.sureName = sureName;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+    public void setPressure(int pressure){
+        this.pressure = pressure;
+    }
+    public void setStepsByDay(int stepsByDay){
+        this.stepsByDay = stepsByDay;
+    }
+    public void printAccountInfo(){
+        System.out.println(this);
+
+    }
+
+    @Override
+    public String toString() {
+        return "AccountInfo:\n" +
+                "name " + this.name + "\n" +
+                "dayOfBirth " + this.dayOfBirth + "\n" +
+                "monthOfBirth " + this.mounthOfBirth + "\n" +
+                "yearOfBirth " + this.yearOfBirth + "\n" +
+                "email " + this.email + "\n" +
+                "phone " + this.phoneNumber + "\n" +
+                "surname " + this.sureName + "\n" +
+                "weight " + this.weight + "\n" +
+                "pressure " + this.pressure + "\n" +
+                "stepsByDay " + this.stepsByDay + "\n" +
+                "age " + this.age + "\n" + "\n";
+    }
+}
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Account john =  new Account("John", 13,11,1995,"John@gmai.com","1234","John",90, 120, 10000 );
+       john.printAccountInfo();
+       Account bob = new Account("Bob",20,10,2000,"Bob@gmail.com","3321","Bob", 100,125, 15000);
+       bob.printAccountInfo();
+       Account jack = new Account("Jack",30,06,1990,"jack@mail.com", "4563","Jack", 70,130,8000);
+       jack.printAccountInfo();
+       jack.setSureName("other name ");
+       jack.setWeight(85);
+       jack.setPressure(120);
+       jack.printAccountInfo();
+
     }
+
+
 }
